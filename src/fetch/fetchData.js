673dc12c7e04785167dd16c7d513083524c07b98
igorcreatievegasten <89772112+igorcreatievegasten@users.noreply.github.com>
2021-09-07 22:25:10 +0200

    ommit

--- src/fetch/fetchData.js
+++ src/fetch/fetchData.js
@@ -1,20 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 
-// function FetchData(page, id) {
-//     const [data, setData] = useState(null);
-
-//     fetch(`http://localhost:1337/${page}/${id}`)
-//           .then((response) => response.json())
-//           .then(setData);
-//     return data
-// }
-
-// async function FetchThatData(page, id) {
-//     const response = await fetch(`http://localhost:1337/${page}/${id}`);
-//     const data = await response.json();
-//     return data;
-// }
 
 const useApi = url => {
     const [data, setData] = useState([]);
